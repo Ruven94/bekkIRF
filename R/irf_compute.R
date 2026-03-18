b685f5ff7cc250873ee225a4136c63f183c74cae
Ruven94 <Ruven.Zapf@stud.uni-goettingen.de>
2026-03-18 14:21:35 +0100

    inital commit

--- R/irf_compute.R
+++ R/irf_compute.R
@@ -1,4 +1,14 @@
+#' Compute an impulse response function
+#'
+#' @param x A numeric input
+#'
+#' @returns A numeric value.
+#' @export
+#'
+#' @examples
+#' x <- 3
+#' irf_compute(x)
+
 irf_compute <- function(x){
   return(x^2 + x)
 }
-
---
+++ tests/testthat/test-irf_compute.R
@@ -0,0 +1,3 @@
+test_that("multiplication works", {
+  expect_identical(irf_compute(3), 3^2 + 3)
+})
